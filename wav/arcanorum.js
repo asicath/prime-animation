5bad8a66cceff7792d69fbb653146c265abb2b94
Scott Wilde <scottpaulwilde@gmail.com>
2020-11-02 22:16:38 -0800

    tones 2

--- wav/arcanorum.js
+++ wav/arcanorum.js
@@ -1,59 +1,198 @@
 const {writeWav, createSilence, generateAllNotes, getAllNotes, createTone} = require('./wav');
 
+const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+
 const sampleRate = 48000; // 96000;
 
 const notes = getAllNotes();
-const duration = 1000*10;
-const primary = notes['A1'].freq;
-const maxAmp = 32767;
+const duration = 1000*10*1;
 
-let combined = null;
+const maxAmp = 32767-1000;
 
-for (let n = 1; n <= 22; n++) {
+//makeWavs1();
 
-    // create tone
-    const data = getTone(primary*n);
 
-    // output
-    const filename = 'arcanorum-output/' + (n < 10 ? "0" + n.toString() : n) + ".wav";
-    output(data, filename);
+const chords = [
+    //['A1','C1','E1'], // a minor
+    //['C1','A1','E1'], // a minor
 
-    // combine
-    if (combined === null) {
-        combined = data;
+    ['C1','E1','G1'], // a minor
+    //['G1','C1','E1'], // a minor
+
+/*    ['C1','E1','G1'], // C major
+    ['D1','F1','A1'], // d minor
+    ['E1','G1','B1'], // e minor
+    ['F1','A1','C1'], // F major
+    ['G1','B1','D1'], // G major
+
+    ['B1','D1','F1'], // b minor diminished*/
+
+    //['A2','C2','E2'], // a minor
+]
+
+// a minor
+// ACE
+// CAE
+// CEA
+
+// c major
+// CEG
+// GCE **
+
+
+chords.forEach(chord => {
+    makeWavs2([chord[0], chord[1], chord[2]]);
+    //makeWavs2([chord[0], chord[2], chord[1]]);
+    //makeWavs2([chord[1], chord[0], chord[2]]);
+    //makeWavs2([chord[1], chord[2], chord[0]]);
+    //makeWavs2([chord[2], chord[0], chord[1]]);
+    //makeWavs2([chord[2], chord[1], chord[0]]);
+})
+
+
+function makeWavs2(chord) {
+
+
+    function addToCombined(combined, data, ampMod = 1) {
+        if (combined === null) {
+            //combined = data;
+            return data.map((f, i) => f * ampMod);
+        }
+        else {
+            return combined.map((f, i) => f + data[i] * ampMod);
+        }
     }
-    else {
-        combined = combined.map((f, i) => f + data[i]);
+
+    function createNote(primary, count) {
+        let combined = null;
+        for (let n = 1; n <= count; n++) {
+            // create tone
+            const freq = primary * n;
+            const data = getToneRandom(freq);
+
+            // output
+            const filename = `arcanorum-output/part-${count}-${(n < 10 ? "0" + n.toString() : n)}.wav`;
+            output(data, filename, maxAmp);
+
+            let p = (1 - (n-1) / (count-1));
+            p = p*p*p;
+            let ampMod = p * 0.6 + 0.4;
+            combined = addToCombined(combined, data, ampMod);
+        }
+        return combined;
     }
+
+    const freq3 = notes[chord[0]].freq;
+    const freq2 = notes[chord[1]].freq;
+    const freq1 = notes[chord[2]].freq;
+
+    const c1 = createNote(freq1, 3);
+    const c2 = createNote(freq2, 7);
+    const c3 = createNote(freq3, 12);
+
+    output(c1,`arcanorum-output/all1_${chord.join('-')}.wav`, maxAmp)
+    output(c2,`arcanorum-output/all2_${chord.join('-')}.wav`, maxAmp)
+    output(c3,`arcanorum-output/all3_${chord.join('-')}.wav`, maxAmp)
+
+    const combined = c1.map((f, i) => {
+        return f + c2[i] + c3[i];
+    });
+
+    // now output the combined
+    output(combined,`arcanorum-output/all_${chord.join('-')}.wav`, maxAmp)
 }
 
-// now output the combined
+function makeWavs1() {
+    let combined = null;
+    const primary = notes['A1'].freq;
 
-const maxValue = combined.reduce((max, f) => Math.max(max, f));
-const maxAmpCombined = maxAmp / maxValue;
-const dataCombined = combined.map(f => f * maxAmpCombined);
-const filename = 'arcanorum-output/all.wav';
+    for (let n = 1; n <= 22; n++) {
 
-console.log('writing ' + filename);
-writeWav(dataCombined, filename, sampleRate);
+        // create tone
 
-function output(data, filename) {
+        const freq = primary * n;
+        //let cyclesPerSecond = 1 / (freq / 55);
+        //const prime = primes[n-1];
+        //const data = getTone(freq, prime);
+        const data = getToneRandom(freq);
+
+        // output
+        const filename = 'arcanorum-output/' + (n < 10 ? "0" + n.toString() : n) + ".wav";
+        output(data, filename, maxAmp);
+
+        // combine
+        if (combined === null) {
+            combined = data;
+        }
+        else {
+            combined = combined.map((f, i) => f + data[i]*((44-n)/44));
+        }
+    }
+
+    // now output the combined
+    output(combined,'arcanorum-output/all.wav', maxAmp)
+}
+
+
+
+
+function output(data, filename, maxAmplitude) {
     // normalize amplitude
     const maxValue = data.reduce((max, f) => Math.max(max, f));
     const dataNormal = data.map(f => f / maxValue);
     // add volume
-    const dataWithVolume = dataNormal.map(f => Math.floor(f * maxAmp));
+    const dataWithVolume = dataNormal.map(f => Math.floor(f * maxAmplitude));
     console.log('writing ' + filename);
     writeWav(dataWithVolume, filename, sampleRate);
 }
 
-function getTone(freq) {
+function getTone(freq, prime) {
+    let data = createTone(sampleRate, duration, freq, 1);
+
+    const cyclesPerSecond = 3;
+    const samplesPerCycle = sampleRate / cyclesPerSecond;
+
+    data = data.map((f, i) => {
+
+        let cycle = Math.floor(i/samplesPerCycle) + 1;
+
+        if (cycle <= prime) return 0;
+
+        // non-active
+        if (cycle % prime !== 0) return f;
+
+        // determine what part of the cycle
+        let percent = (i % samplesPerCycle) / samplesPerCycle;
+        let amplitude = 1;
+        // attack
+        if (percent < 0.4) {
+            percent = percent / 0.4;
+            amplitude = percent*percent;
+        }
+        // release
+        else if (percent > 0.6) {
+            percent = (1-percent) / 0.4;
+            amplitude = percent*percent;
+        }
+
+        return f * (1+amplitude*5);
+
+        //let angle = percent * Math.PI; // just half
+        //let amplitude = Math.sin(angle);
+
+        //return f * (1 + amplitude * 10);
+    });
+
+    return data;
+}
+
+function getToneRandom(freq) {
     let data = createTone(sampleRate, duration, freq, 1);
 
     let delta = 0;
     const step = 0.01;
-    const threshold = 0.1;
-    const maxDelta = 1;
+    const threshold = 0.3;
+    const maxDelta = 0.9;
     data = data.map(f => {
 
         let r = Math.random();
